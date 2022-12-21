035efeae807963d176a5c2114bc74da642a0bed7
서윤하 <bobo9245@naver.com>
2022-12-21 10:10:24 +0000

    221221 로그인 페이지 완성

--- login_form/script.js
+++ login_form/script.js
@@ -1,4 +1,20 @@
 
-var submit = () =>{
-	alert('회원가입이 완료되었습니다.');
+function submit(){
+	const name = document.getElementById('name').value;
+	console.log(name==='');
+	const id=document.getElementById('id').value;
+	const pw=document.getElementById('pw').value;
+	const pw_validate = document.getElementById('pw_validate').value;
+	const telephone = document.getElementById('telephone').value;
+	if(name===''||id===''||pw===''||pw_validate==''||telephone==='')
+		alert('값을 모두 입력해 주세요.');
+	else if(pw!==pw_validate)
+		alert('비밀번호가 일치하지 않습니다.');
+	else if(telephone.length!=11)
+		alert('알맞은 전화번호 양식으로 입력해 주세요.');
+	else{
+		const data = [id,pw,telephone];
+		window.localStorage.setItem(name,JSON.stringify(data));
+		alert('회원가입이 완료되었습니다.');
+	}
 }
