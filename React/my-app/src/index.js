9c0d017e7493b4a129c3f766f74e938edb0f6e2b
서윤하 <bobo9245@naver.com>
2023-03-14 14:21:20 +0000

    230314 리액트 가지고 놀기 1일차

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Main_Page} from './Components/Main_Page/Main_Page';
+import "./assets/fonts/fonts.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main_Page/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
